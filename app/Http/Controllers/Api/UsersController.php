301907cf322538a0594c152dff39c7c5b6e473b7
CrowdYellow <echo61546@gmail.com>
2019-09-21 16:42:16 +0800

    V1.0.4 构建需要登陆的接口

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Requests\Api\UserRequest;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    public function index()
+    public function store(UserRequest $request)
     {
         return $this->response->array(['test_message' => 'store verification code']);
+    }
+
+    public function me()
+    {
+        return $this->response->item($this->user(), new UserTransformer());
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,23 @@ $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api'
 ], function($api) {
-    # 测试接口
-    $api->get('users', 'UsersController@index')
-        ->name('api.users.store');
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.sign.limit'),
+        'expires' => config('api.rate_limits.sign.expires'),
+    ], function($api) {
+        # 游客可以访问的接口
+        # 用户注册
+        $api->get('users', 'UsersController@store')
+            ->name('api.users.store');
+
+        # 需要 token 验证的接口
+        $api->group(['middleware' => 'api.auth'], function($api) {
+            // 当前登录用户信息
+            $api->get('user', 'UsersController@me')
+                ->name('api.user.show');
+        });
+    });
 });
 
 $api->version('v2', function($api) {
